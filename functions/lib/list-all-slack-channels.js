40ccb06c8ebcb7d4e21f3b2936a0d252ab6b5a69
Adam Braimbridge <adam.braimbridge@ft.com>
2020-04-23 14:26:33 +0100

    List all channels POC

--- functions/lib/list-all-slack-channels.js
+++ functions/lib/list-all-slack-channels.js
@@ -1,31 +1,55 @@
 const { WebClient } = require('@slack/web-api')
 const slackClient = new WebClient(process.env.BOT_USER_ACCESS_TOKEN)
 
-const getModalJson = ({ view_id, callback_id }) => ({
-	view_id,
-	view: {
-		callback_id,
-		type: 'modal',
-		title: {
-			type: 'plain_text',
-			text: `All Slack channels`,
-		},
-		blocks: [
+const getSlackChannels = async () => {
+	const response = await slackClient.conversations //
+		.list({
+			types: 'public_channel',
+			exclude_archived: true,
+			limit: 1000,
+		})
+		.catch(console.error)
+	return response
+}
+
+const getModalJson = ({ view_id, callback_id, channels }) => {
+	const blocks = channels.map((channel) => {
+		return [
 			{
 				type: 'section',
 				block_id: 'slack-channel-list',
 				text: {
 					type: 'mrkdwn',
-					text: `Here's a list all Slack channels`,
+					text: `${channel.name_normalized} — ${channel.purpose.value}`,
 				},
 			},
-		],
-	},
-})
+		]
+	})
+
+	return {
+		view_id,
+		view: {
+			callback_id,
+			type: 'modal',
+			title: {
+				type: 'plain_text',
+				text: `Channel Tuna: All Slack channels`,
+			},
+			blocks,
+		},
+	}
+}
 
 const listAllSlackChannels = async ({ view_id, callback_id }) => {
+	const { channels } = await getSlackChannels()
 	const response = await slackClient.views //
-		.update(getModalJson({ view_id, callback_id }))
+		.update(
+			getModalJson({
+				view_id,
+				callback_id,
+				channels,
+			})
+		)
 		.catch(console.error)
 
 	return response
